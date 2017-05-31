d1b3ac64de70d7bec4c462a18793a40e81617c03
cassaram09 <cassaram09@gmail.com>
2017-05-31 12:46:44 +0100

    add sample DOB data and stub in App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { data } from './data.js'
 
 class App extends Component {
   constructor(){
     super()
 
-    this.state={
-      complaints: []
+    this.state = {
+      complaints: data
     }
   }
 
   componentDidMount(){
-    fetch('https://data.cityofnewyork.us/resource/muk7-ct23.json?$$app_token=zA2KvXZaMng7Jr0mNekDIChOA').then(
-      response =>{
-        console.log(response)
-        return response.json()
-      }).then( response => {
-        console.log(response)
-        this.setState({complaints: response})
-      })
+    // fetch('https://data.cityofnewyork.us/resource/muk7-ct23.json?$$app_token=zA2KvXZaMng7Jr0mNekDIChOA').then(
+    //   response =>{
+    //     console.log(response)
+    //     return response.json()
+    //   }).then( response => {
+    //     console.log(response)
+    //     this.setState({complaints: response})
+    //   })
   }
 
   render() {
     const complaints = this.state.complaints.map( (complaint, index) =>{
-      return <div key={index}> {complaint.status} </div>
+      return <div key={index}> {complaint.Status} </div>
     })
     return (
       <div className="App">
